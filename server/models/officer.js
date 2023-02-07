5244df91aad4a1dd4718ddbe239220d52ec6178e
natty-7 <nati7fekadu@gmail.com>
2023-02-07 20:56:07 +0300

    workin on auth and officer

---
+++ server/models/officer.js
@@ -0,0 +1,37 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../configs/dbConfig");
+
+const Officer = sequelize.define(
+  "officers",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = Officer;
