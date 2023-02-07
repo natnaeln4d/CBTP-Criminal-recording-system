508f5efcf8248edc5c6552e2aef96e14b0c38d59
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-02-07 22:24:55 +0300

    Modified admin routes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const adminController = require("../controllers/admin");
 
+router.get("/criminals", adminController.getCriminals);
 router.post("/addCriminal", adminController.addCriminal);
 
 module.exports = router;
