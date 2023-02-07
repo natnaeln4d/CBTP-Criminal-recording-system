2faf69e168470295c85d85c09fcc70ff9eafa6a0
Natnael Getachew <natnaeln4d@gmail.com>
2023-02-07 19:46:21 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import SectionContainer from './componets/Container/Section/SectionContainer';
+import Viewall from './componets/Container/Viewall/Viewall';
 import Footer from './componets/Footer/Footer';
 import Login from './componets/login/Login';
 import Navbar from './componets/Navbar/Navbar';
@@ -9,9 +10,11 @@ function App() {
   return (
     <div className="w-[100%]">
       <Navbar />
-      <SectionContainer />
-      {/* <Sidebar /> */}
-      <Footer />
+      {/* <SectionContainer /> */}
+      
+      <Viewall />
+      {/* <Sidebar />
+      <Footer /> */}
       {/* <Login /> */}
       
    
--- src/componets/Container/Profile/AdminPhoto.js
+++ src/componets/Container/Profile/AdminPhoto.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import adminPhoto from '/home/kuusaa/Documents/VS/crm/CBTP-Criminal-recording-system/src/componets/Photo/rob--profile.jpg'
+// import React from 'react'
+// import adminPhoto from '/src/componets/Photo/rob--profile.jpg'
 
 
-function AdminPhoto() {
-  return (
-    <div className="admin_photo">
-            <img src={adminPhoto} alt="" />
-        </div>
-  )
-}
+// function AdminPhoto() {
+//   return (
+//     <div className="admin_photo">
+//             <img src={adminPhoto} alt="" />
+//         </div>
+//   )
+// }
 
-export default AdminPhoto
+// export default AdminPhoto
