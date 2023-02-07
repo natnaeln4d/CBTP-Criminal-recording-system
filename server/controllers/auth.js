fd7c66bc87fdf749a049921288dd30d92455cb71
natty-7 <nati7fekadu@gmail.com>
2023-02-07 21:19:33 +0300

    _

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,17 +4,17 @@ const SuperAdmin = require("../models/admin");
 const Officer = require("../models/officer");
 
 exports.postLogin = async (req, res, next) => {
-  // console.log(req.body);
-  // return;
   const { email, password } = req.body;
 
   const user = await Auth.findOne({ where: { email: email } });
+  const users = await Auth.findOne();
 
   if (!user) {
     return res.status(401).json("Invalid email");
   }
-  req.session.user = user;
-  const match = bcrypt.compare(password, user.password);
+  // const match = bcrypt./compare(password, user.password);
+  console.log(password);
+  const match = password == user.password;
   if (match) {
     let userData;
     if (user.role == "superadmin") {
@@ -32,24 +32,7 @@ exports.postLogin = async (req, res, next) => {
     return res.status(403).json({ auth: false, msg: "access denied" });
   }
 };
-exports.logout = async (req, res, next) => {
-  const { email, password } = req.body;
 
-  const user = await Auth.findOne({ where: { email: email } });
-
-  if (!user) {
-    return res.status(401).json("Invalid email");
-  }
-  req.session.user = user;
-  const match = bcrypt.compare(password, user.password);
-  if (match) {
-    const userData = await SuperAdmin.findOne({ where: { email: user.email } });
-
-    return res.status(200).json({ auth: true, userData: userData });
-  } else {
-    return res.status(403).json({ auth: false, msg: "access denied" });
-  }
-};
 exports.logout = async (req, res, next) => {
   try {
     req.session.destroy((user) => {
