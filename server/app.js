c98a50636ee453d856465bbc696307febd553686
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-02-07 21:10:36 +0300

    Fixed officer routing

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const sequelize = require("./configs/dbConfig");
 
-//
 const officerRoutes = require("./routes/officer");
 const authRoutes = require("./routes/auth");
 const superAdminRoutes = require("./routes/superAdmin");
@@ -25,7 +24,7 @@ app.use("/", authRoutes);
 const PORT = process.env.PORT || 5000;
 
 sequelize
-  .sync({ alter: true })
+  .sync()
   // .sync({ force: true })
   .then()
   .catch((err) => console.log(err));
