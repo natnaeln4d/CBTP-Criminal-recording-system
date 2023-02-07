6cfd943774425ad199f74732c97af1f999fd4fbc
natty-7 <nati7fekadu@gmail.com>
2023-02-07 21:49:40 +0300

    _

--- server/routes/superAdmin.js
+++ server/routes/superAdmin.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
-const superAdminController = require("../controllers/superAdmin");
+
+const superAdminController = require("../controllers/superadmin");
+
 router.post("/addUser", superAdminController.addUser);
+
 router.post("/criminal/:criminalId", superAdminController.searchCriminal);
-router.post("/admin", superAdminController.searchCriminal);
+
+// router.post("/admin", superAdminController.searchCriminal);
 
 module.exports = router;
