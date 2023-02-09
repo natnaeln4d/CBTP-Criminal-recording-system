e13262e04f6850f03e5c78d2656a1b7ff42b4345
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-02-09 22:09:19 +0300

    Delete middleware directory

--- middleware/isAuth.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-  //If the user isnot authenticated, redirect to login
-  if (!req.session.isLoggedIn) {
-    return res.redirect("/login");
-  }
-  next();
-};
