ab0b7b770df00e18486b8f27f92a84a344ed1aca
Nati <natnael3370@gmail.com>
2023-02-10 03:07:55 +0300

    0

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import './App.css';
-import SectionContainer from './componets/Container/Section/SectionContainer';
-import Viewall from './componets/Container/Viewall/Viewall';
-import Footer from './componets/Footer/Footer';
-import Login from './componets/login/Login';
-import Navbar from './componets/Navbar/Navbar';
-import Sidebar from './componets/SIdebar/Sidebar';
+import "./App.css";
+import SectionContainer from "./componets/Container/Section/SectionContainer";
+import Viewall from "./componets/Container/Viewall/Viewall";
+import Footer from "./componets/Footer/Footer";
+import Login from "./componets/login/Login";
+import Navbar from "./componets/Navbar/Navbar";
+import Sidebar from "./componets/SIdebar/Sidebar";
 
 function App() {
   return (
     <div className="w-[100%]">
       <Navbar />
-      <SectionContainer />
+      <Viewall />
+      {/* <SectionContainer /> */}
       {/* <Sidebar /> */}
-      <Footer />
-       {/* <Login />  */}
-      
-   
+      {/* <Footer /> */}
+      {/* <Login /> */}
     </div>
   );
 }
